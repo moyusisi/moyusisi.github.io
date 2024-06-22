222096b545ce69f67dc878665654ffa6da89845d
moyusisi <moyusisi@126.com>
2024-06-23 03:36:05 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -8,8 +8,8 @@ export default hopeTheme({
   hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
 
   author: {
-    name: "Mr.Hope",
-    url: "https://mister-hope.com",
+    name: "moyusisi",
+    // url: "https://mister-hope.com",
   },
 
   iconAssets: "fontawesome-with-brands",
