ac0ae48027d287aeb857a91a5ba93aa8929b76a4
moyusisi <moyusisi@126.com>
2024-06-23 22:41:23 +0800

    navbar

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -5,6 +5,24 @@ export default navbar([
   "/portfolio",
   // NavbarGroup
   {
+    text: '编程',
+    prefix: '/program/',
+    children: [
+      { text: '后端技术', link: 'backend/' },
+      { text: '前端技术', link: 'frontend/' },
+      {
+        text: "运维技术",
+        prefix: "ops/",
+        children: [
+          { text: "Linux", link: "linux/" },
+          { text: "MacOS", link: "macos/" },
+          { text: "数据库", link: "db/" },
+        ],
+      }
+    ]
+  },
+  // NavbarGroup
+  {
     text: '算法',
     prefix: '/algorithm/',
     children: [
